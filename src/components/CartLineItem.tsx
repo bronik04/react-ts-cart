e48b35197ebb7c55309f210cea78d327d290d12c
Nikolay Brovkin <bronik004@gmail.com>
2023-01-26 13:34:28 +0300

    feat: updated CartLineItem

--- src/components/CartLineItem.tsx
+++ src/components/CartLineItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent, ReactElement} from 'react';
 import {CartItemType} from "../context/CartProvider";
 import {ReducerAction} from "../context/CartProvider";
 import {ReducerActionType} from "../context/CartProvider";
@@ -13,11 +13,56 @@ const CartLineItem = ({item, dispatch, REDUCER_ACTIONS}: PropsType) => {
     const img: string = new URL(`../images/${item.sku}.jpg`, import.meta.url).href;
     const lineTotal: number = item.qty * item.price;
     const highestQty: number = 20 > item.qty ? 20 : item.qty;
-    const optionValues: number[] = [...Array(highestQty).keys()].map(i => i +1)
-    return (
-        <div>
+    const optionValues: number[] = [...Array(highestQty).keys()].map(i => i + 1);
+    const options: ReactElement[] = optionValues.map(val => {
+        return <option key={`opt${val}`} value={val}>{val}</option>
+    });
+    const onChangeQty = (e: ChangeEvent<HTMLSelectElement>) => {
+        dispatch({
+            type: REDUCER_ACTIONS.QUANTITY,
+            payload: {...item, qty: Number(e.target.value)}
+        })
+    }
 
-        </div>
+    const onRemoveFromCart = () => dispatch({
+        type: REDUCER_ACTIONS.REMOVE,
+        payload: item,
+    })
+    return (
+        <li className={'cart__item'}>
+            <img src={img} alt={item.name} className="cart__img"/>
+            <div aria-label={'item name'}>{item.name}</div>
+            <div aria-label={'price per item'}>{new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD'
+            }).format(item.price)}</div>
+            <label className={'offscreen'} htmlFor={'itemQty'}>
+                ItemQuantity
+            </label>
+            <select
+                name="itemQty"
+                id="itemQty"
+                className={'cart__select'}
+                value={item.qty}
+                aria-label={'item quantity'}
+                onChange={onChangeQty}
+            >{options}</select>
+            <div className="cart__item-subtotal" aria-label={'line item' +
+                ' subtotal'}>
+                {new Intl.NumberFormat('en-US', {
+                    style: 'currency',
+                    currency: 'USD'
+                }).format(lineTotal)}
+            </div>
+            <button
+                className={'cart__button'}
+                aria-label={'remove item from the cart'}
+                title={'remove item from the cart'}
+                onClick={onRemoveFromCart}
+            >
+                ×
+            </button>
+        </li>
     );
 };
 
